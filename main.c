bb49dc428c0dc61d97dfab306f280b530e5604d0
EnriMinut <12999495@itisrossi.vi.it>
2025-09-30 22:13:32 +0200

    Aggiunta struct Contatto

--- main.c
+++ main.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_CONATTATTI 100
+
+struct Contatto{
+    char nome[30];
+    char cognome[30];
+    char telefono[10];
+};
 
 int main(void) {
     printf("Hello, World!\n");
